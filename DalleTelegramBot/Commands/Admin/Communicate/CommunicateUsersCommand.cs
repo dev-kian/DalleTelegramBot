560be737de71378de175c79b82810834f25e8391
dev-kian <kianshabanpourr@gmail.com>
2023-05-10 14:55:17 +0330

    Fix rate limit from telegram api

--- DalleTelegramBot/Commands/Admin/Communicate/CommunicateUsersCommand.cs
+++ DalleTelegramBot/Commands/Admin/Communicate/CommunicateUsersCommand.cs
@@ -6,7 +6,10 @@ using DalleTelegramBot.Common.Extensions;
 using DalleTelegramBot.Common.IDependency;
 using DalleTelegramBot.Common.Utilities;
 using DalleTelegramBot.Data.Contracts;
+using DalleTelegramBot.Data.Models;
 using DalleTelegramBot.Services.Telegram;
+using Serilog;
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 
@@ -29,26 +32,43 @@ internal class CommunicateUsersCommand : BaseCommand, IScopedDependency
 
         if(_cache.CanGetLastCommand(userId, "communicate-users", 1, true))
         {
-            var usersBanned = await _userRepository.GetAllAsync(isBan: false);
-            await _telegramService.SendMessageAsync(userId, string.Format(TextUtility.CommunicateCommandStartForwardingMessageFormat, usersBanned.Count()).EscapeMarkdownV2(), ParseMode.MarkdownV2, cancellationToken);
-            var usersChunks = usersBanned.Chunk(100);
+            var users = await _userRepository.GetAllAsync(isBan: false);
+            await _telegramService.SendMessageAsync(userId, string.Format(TextUtility.CommunicateCommandStartForwardingMessageFormat, users.Count()).EscapeMarkdownV2(), ParseMode.MarkdownV2, cancellationToken);
             int sent = 0, failed = 0;
-            foreach (var usersChunk in usersChunks)
+            int batchSize = 30; // send messages to 30 users at a time
+            int delaySeconds = 2; // wait 2 seconds between batches
+
+            for (int i = 0; i < users.Count(); i += batchSize)
             {
-                var tasks = usersChunk.Select(x => Task.Run(async () =>
+                var batch = users.Skip(i).Take(batchSize).ToList();
+                var tasks = batch.Select(user => _telegramService.ForwardMessageAsync(user.Id, userId, message.MessageId, cancellationToken)).ToList();
+
+                try
                 {
-                    try
+                    await Task.WhenAll(tasks);
+                    sent += tasks.Count;
+                }
+                catch (AggregateException ex)
+                {
+                    Log.Error(ex.Flatten(), "EX in Froward to all users");
+
+                    foreach (var innerEx in ex.InnerExceptions)
                     {
-                        await _telegramService.ForwardMessageAsync(x.Id, userId, message.MessageId, cancellationToken);
-                        Interlocked.Increment(ref sent);
+                        if (innerEx is ApiRequestException apiEx && apiEx.ErrorCode == 429)
+                        {
+                            int retryAfterSeconds = apiEx.Parameters != null && apiEx.Parameters.RetryAfter != null ? apiEx.Parameters.RetryAfter.Value : delaySeconds;
+                            await Task.Delay(retryAfterSeconds * 1000, cancellationToken);
+                        }
+                        else
+                        {
+                            failed++;
+                        }
                     }
-                    catch
-                    {
-                        Interlocked.Increment(ref failed);
-                    }
-                }));
-                await Task.WhenAll(tasks);
+                }
+
+                await Task.Delay(delaySeconds * 1000, cancellationToken);
             }
+
 
             await _telegramService.SendMessageAsync(userId, string.Format(TextUtility.CommunicateCommandEndForwardMessageFormat, sent, failed).EscapeMarkdownV2(), ParseMode.MarkdownV2, cancellationToken);
         }
